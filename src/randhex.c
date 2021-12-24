c35e2e0728332d1f70edea93785b0e3d4effc5fe
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-24 23:46:03 +0800

    feat: Add command-line parsing to randhex

--- src/randhex.c
+++ src/randhex.c
@@ -1,13 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <getopt.h>
+
+static int help_flag;
+
+
+void
+PrintHelp (char **argv)
+{
+  printf(
+    "Usage: %c [OPTIONS]\n\n"
+
+    "options:\n"
+    "-h, --help     Print help and exit\n"
+    "-l, --length   Length of hexadecimal\n"
+    "               numbers to print\n"
+  ,*argv[0]);
+};
+
 int
 main (int argc, char** argv)
 {
-  if(argc==1)
-  {
-    printf("Usage: %c", *argv[0]);
-    return 1;
+  int c;
+  int hexlen = 0;
+
+  for (;;) {
+    static struct option longopts[] = {
+    {"length", required_argument, 0, 'l'},
+    {"help",   no_argument, &help_flag, 1}};
+    int optindex = 0;
+    c = getopt_long (argc, argv, "hl:",
+                     longopts, &optindex);
+    if(c==-1){break;}
+    switch(c)
+    {
+      case 'h':
+        help_flag = 1;
+        break;
+      case 'l':
+        hexlen = atoi(optarg);
+        break;
+    }
+  }
+
+  if(help_flag){PrintHelp(argv);return 0;};
+
+  if(hexlen==0){
+    printf("fatal: length of hex not specified\n"
+           "try: %s -l 40",argv[0]);
+    exit (1);
   }
 
   srand(clock());
@@ -18,7 +60,7 @@ main (int argc, char** argv)
      '2', '3', '4', '5',
      '6', '7', '8', '9'};
 
-  for(int i = 0; i < atoi(argv[1]); i++)
+  for(int i = 0; i < hexlen; i++)
   {
     printf("%c",hex[rand()%16]);
   }
