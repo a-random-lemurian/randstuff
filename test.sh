4d020f22bf12c55953240eab163dba33d620c58a
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-26 00:55:07 +0800

    feat(tests): Revamp tests
    - Added tests for randascii and randfromargs
    - Save tests to a file
    - Print out test output for GH actions

--- test.sh
+++ test.sh
@@ -1,7 +1,27 @@
 #! /usr/bin/bash
-echo "BEGIN TEST... $(date --iso-8601=ns)"
-./bin/randhex -l1000 > /dev/null
-./bin/randhex -h > /dev/null
-./bin/randhex --upper -l1000 > /dev/null
-./bin/randhex --mixed -l1000 > /dev/null
-echo "END TEST...   $(date --iso-8601=ns)"
+TESTOUTPUT="./log/Test-All-$(date --iso-8601=ns).log"
+echo "===================================< BEGIN TEST... $(date --iso-8601=ns)"
+
+echo "----------> Begin test of randhex...               $(date --iso-8601=ns)"
+./bin/randhex -l1000 >> $TESTOUTPUT;                            echo "Ran test [0]"
+./bin/randhex -h >> $TESTOUTPUT;                                echo "Ran test [1]"
+./bin/randhex --upper -l1000 >> $TESTOUTPUT;                    echo "Ran test [2]"
+./bin/randhex --mixed -l1000 >> $TESTOUTPUT;                    echo "Ran test [3]"
+echo "----------> End test of randhex...                 $(date --iso-8601=ns)"
+
+echo "----------> Begin test of randascii...             $(date --iso-8601=ns)"
+./bin/randascii -l100 >> $TESTOUTPUT;                           echo "Ran test [0]"
+./bin/randascii -h >> $TESTOUTPUT;                              echo "Ran test [1]"
+./bin/randascii -l100 --alphanumeric >> $TESTOUTPUT;            echo "Ran test [2]"
+echo "----------> End test of randascii...               $(date --iso-8601=ns)"
+
+echo "----------> Begin test of randfromargs...          $(date --iso-8601=ns)"
+./bin/randfromargs -a01 -l100 >> $TESTOUTPUT;                   echo "Ran test [0]"
+./bin/randfromargs -h >> $TESTOUTPUT;                           echo "Ran test [1]"
+./bin/randfromargs -a acgt -l100 >> $TESTOUTPUT;                echo "Ran test [2]"
+echo "----------> End test of randfromargs...            $(date --iso-8601=ns)"
+
+
+echo "===================================< END TEST...   $(date --iso-8601=ns)"
+# For GitHub actions
+cat $TESTOUTPUT
