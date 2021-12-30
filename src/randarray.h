9fb7423e8602cde219a946277b38326be69a31f2
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-30 23:08:30 +0800

    feat: Buffer printing of characters for speed
    By buffering printf() by declaring a char [] of a specified block
    size based on the requested length of random characters, and
    filling the char [] with random characters and then feeding it to
    printf(), printing can be done faster, hopefully.

--- src/randarray.h
+++ src/randarray.h
@@ -26,10 +26,58 @@
 inline extern void RandomArray_char(int arraySize, int offset, int samplesize,
                                     char chararray[])
 {
-	for (int i = 0; i < samplesize; i++) {
+	/* Based on the samplesize, instead define strings of a set size
+	 * based on samplesize. Then flush these strings out of the buffer,
+	 * for faster printing.
+	 */
+
+	int blksiz = 10; // Block size, default is 10
+	int runoff = 0; // Runoff that doesn't fit in the blocks
+
+
+	if (samplesize > 10000)       blksiz = 1000;
+	if (samplesize > 100000)      blksiz = 5000;
+	if (samplesize > 1000000)    blksiz = 10000;
+	if (samplesize > 10000000)   blksiz = 100000;
+
+
+	while (samplesize % blksiz != 0)
+	{
+		samplesize--;
+		runoff++;
+	}
+
+	int i;
+
+	for (i = 0; i < runoff; i++) {
 		printf("%c", chararray[offset + rand() % arraySize]);
 	}
+
+	i = 0;
+
+	char blk[blksiz+1];
+
+	blk[blksiz] = '\0';
+
+	int idx;
+
+	for (int loops = 0; loops < samplesize; loops += blksiz)
+	{
+
+
+		for (idx = 0; idx < blksiz; idx++)
+		{
+			blk[idx] = chararray[offset+ rand() % arraySize];
+		}
+
+		idx = 0;
+		printf("%s", blk);
+
+	}
+
+
 	puts ("\n");
+	printf("blksiz %d",blksiz);
 }
 
 #endif
